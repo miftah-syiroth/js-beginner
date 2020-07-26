9bd16b8137d26afab652e48b72cb386545c2f385
miftah-syiroth <irothsyiroth@gmail.com>
2020-07-26 10:37:49 +0700

    buat file baru:   index.html new file:   test.js

---
+++ test.js
@@ -0,0 +1,21 @@
+// DOM selection
+
+// document.getElementById
+
+// let judul = document.getElementById("judul");
+// judul.style.color = "red";
+// judul.style.backgroundColor = "grey";
+// judul.innerHTML = "Miftakhusy Syiroth";
+
+// document.getElementsByTagName
+
+// let p = document.getElementsByTagName('p');
+
+// for (let key in p) {
+//      p[key].style.backgroundColor = "lightblue";
+// }
+
+// document.querySelector
+let par4 = document.querySelector("#b p");
+par4.style.color = "green";
+par4.style.fontSize = "30px";
